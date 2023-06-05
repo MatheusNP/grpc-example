3faf2a6db578248a5b263a3a65657eab8b569c3d
Matheus Nobre <matheusnobre56@gmail.com>
2023-06-04 21:37:25 -0300

    feat: add create category service

--- go.mod
+++ go.mod
@@ -2,4 +2,17 @@ module github.com/MatheusNP/grpc-example
 
 go 1.20
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/golang/protobuf v1.5.3
+	github.com/google/uuid v1.3.0
+	github.com/mattn/go-sqlite3 v1.14.17
+	google.golang.org/grpc v1.55.0
+)
+
+require (
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ internal/service/category.go
@@ -0,0 +1,32 @@
+package service
+
+import (
+	"context"
+
+	"github.com/MatheusNP/grpc-example/internal/database"
+	"github.com/MatheusNP/grpc-example/internal/pb"
+)
+
+type CategoryService struct {
+	pb.UnimplementedCategoryServiceServer
+	CategoryDB database.Category
+}
+
+func NewCategoryService(categoryDB database.Category) *CategoryService {
+	return &CategoryService{
+		CategoryDB: categoryDB,
+	}
+}
+
+func (c *CategoryService) CreateCategory(ctx context.Context, in *pb.CreateCategoryRequest) (*pb.Category, error) {
+	category, err := c.CategoryDB.Create(ctx, in.Name, in.Description)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Category{
+		Id:          category.ID,
+		Name:        category.Name,
+		Description: category.Description,
+	}, nil
+}
