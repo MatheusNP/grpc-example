dc3926b2139426e9e191b3057474ea77359c182e
Matheus Nobre <matheusnobre56@gmail.com>
2023-06-05 23:11:42 -0300

    feat: add create category stream rpc

--- internal/service/category.go
+++ internal/service/category.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"io"
 
 	"github.com/MatheusNP/grpc-example/internal/database"
 	"github.com/MatheusNP/grpc-example/internal/pb"
@@ -60,4 +61,30 @@ func (c *CategoryService) GetCategory(ctx context.Context, in *pb.GetCategoryReq
 		Name:        category.Name,
 		Description: category.Description,
 	}, nil
+}
+
+func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateCategoryStreamServer) error {
+	ctx := context.TODO()
+	categories := &pb.CategoryList{}
+
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(categories)
+		}
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(ctx, category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		categories.Categories = append(categories.Categories, &pb.Category{
+			Id:          categoryResult.ID,
+			Name:        categoryResult.Name,
+			Description: categoryResult.Description,
+		})
+	}
 }
